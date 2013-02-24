f8a97c5d697f36c01a08061adc4f786ab310bc36
James Coglan <jcoglan@gmail.com>
2013-02-24 09:49:12 +0000

    Parse dates explicitly because old browsers.

--- build/toml.js
+++ build/toml.js
@@ -5067,7 +5067,14 @@ TOML.DocumentParser.Boolean = {
 
 TOML.DocumentParser.Datetime = {
   interpret: function() {
-    return new Date(this.textValue);
+    var date = new Date();
+    date.setFullYear(parseInt(this.year.textValue, 10));
+    date.setMonth(parseInt(this.month.textValue, 10) - 1);
+    date.setDate(parseInt(this.day.textValue, 10));
+    date.setHours(parseInt(this.hour.textValue, 10));
+    date.setMinutes(parseInt(this.minute.textValue, 10));
+    date.setSeconds(parseInt(this.second.textValue + this.fraction.textValue, 10));
+    return date;
   }
 };
 
